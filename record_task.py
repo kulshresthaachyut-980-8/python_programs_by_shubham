cf7d2714d732645af3b984c58ae7a30cc76444f8
kulshresthaachyut-980-8 <kulshresthaachyut@gmail.com>
2026-04-29 11:09:58 +0530

    Minor Mistakes fixed.

--- record_task.py
+++ record_task.py
@@ -4,70 +4,68 @@ main_list=[]
 
 while True:
     
-    employ_data={}
+    employee_data={}
     print("=============================")
-    print("  REGISTER EMPLOY      : '1' ")
-    print("  SEARCH EMPLOY DATA   : '2' ")
-    print("  VIEW EMPLOY DATA     : '3' ")
+    print("  REGISTER EMPLOYEE    : '1' ")
+    print("  SEARCH EMPLOYEE DATA : '2' ")
+    print("  VIEW EMPLOYEE DATA   : '3' ")
     print("  EXIT                 : '4' ")
     print("=============================")
 
-    user_input=input(" Enter Your Choice: ")
-    if len(user_input)==1 and user_input.isdigit():
-
-        if user_input=='1':
-            print("Wellcome to Employ registration Corner.")
+    user_input=(input(" Enter Your Choice: "))
+    choice=int(user_input)
+    
+    
+    if choice==1:
+        print("Welcome to Employee registration Corner.")
             
-            employ_data["NAME"]=input(" Enter Employ's Full Name: ")
-            employ_data["ID"]=input(" Enter Employ ID: ")
-            while True:
-                employ_data["MOBILE"]=input(" Enter Employ's Mobile No. (10 Digits): ")
-                if len(employ_data["MOBILE"])==10 and employ_data["MOBILE"].isdigit():
-                    break
-                else:
-                    print("Invalid Mobile Number ")
-            employ_data["Email ID"]=input(" Enter Employ's Email ID: ")
-
-
-            post_are=["CEO","Manager","Assistant","Developer"]
-            while True:
-                employ_data["POST"]=input(" Enter Employ's Current Post: ")
-                if employ_data["POST"] in post_are:
-                    break
-
-                for posts in post_are:
-                    if posts.lower() == employ_data["POST"].lower():
-                        break
-
-                    else:
-                         print("Post not Found")
-
-                main_list.append(employ_data)
-
-        elif user_input=='2':
-            check_id=input("Enter Employ's ID ")
-            for data_b in main_list:
-                if check_id==data_b["ID"]:
-                    print("\n He is a Valid Employ.")
-                    break
+        employee_data["name"]=input(" Enter Employee's Full Name: ")
+        employee_data["id"]=input(" Enter Employee ID: ")
+        while True:
+            employee_data["mobile"]=input(" Enter Employee's Mobile No. (10 Digits): ")
+            if len(employee_data["mobile"])==10 and employee_data["mobile"].isdigit():
+                break
             else:
-                print(" Employ Data Not Found !")
+                print("Invalid Mobile Number ")
+        employee_data["email_id"]=input(" Enter Employee's Email ID: ")
 
-        elif user_input=='3':
-            check_name=input("Enter Employ's Name : ")
-            check_email=input("Enter Employ's Email Id : ")
-            for data_c in (main_list):
-                if check_name==data_c["NAME"] and check_email==data_c["Email ID"]:
-                    print("\n----------Employ Data------------ ")
-                    print(json.dumps(data_c,indent=4)) #copy
-                    break
+        roles_are=["CEO","Manager","Assistant","Developer"]
+        while True:
+            employee_data["role"]=input(" Enter Employee's Current Post: ")
+            if employee_data["role"] in roles_are:
+                break
             else:
-                print("Employ Not Found") 
+                 print("Post not Found")
 
-        elif user_input=='4':
-            print("Exiting...")
-            break
+        main_list.append(employee_data)
+        print("Employee Registerd Successfully.")
+
+    elif choice==2:
+        check_id=input("Enter Employee's ID ")
+        for data_b in main_list:
+            if check_id==data_b["id"]:
+                print("\n He is a Valid Employee.")
+                break
         else:
-         print("Enter Valid Choice -> ('1','2','3' or '4')")   
+            print(" Employee Data Not Found !")
+
+    elif choice==3:
+        check_name=input("Enter Employee's Name : ").lower()
+        check_email=input("Enter Employee's Email Id : ").lower()
+        for data_c in (main_list):
+            saved_data_name=data_c["name"].lower()
+            saved_data_email=data_c["email_id"].lower()
+
+            if check_name==saved_data_name and check_email==saved_data_email:
+                print("\n----------Employee Data------------ ")
+                print(json.dumps(data_c,indent=4)) #copy
+                break
+        else:
+            print("Employee Not Found") 
+
+    elif choice==4:
+        print("Exiting...")
+        break
     else:
-        print("Only one Digit is valid !")
+        print("Enter Valid Choice -> ('1','2','3' or '4')")   
+   
